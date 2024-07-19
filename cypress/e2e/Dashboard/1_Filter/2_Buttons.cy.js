f183f34e34858b903f9cf7adbde83f1a08567675
AlexSuelzle <145021047+AlexSuelzle@users.noreply.github.com>
2024-07-19 13:39:50 +0200

    update partner auflisten & first version of partner import

--- cypress/e2e/Dashboard/1_Filter/2_Buttons.cy.js
+++ cypress/e2e/Dashboard/1_Filter/2_Buttons.cy.js
@@ -2,7 +2,7 @@
 const baseUrl = Cypress.config('baseUrl')
 
 //generate random mandatory number
-const randomNumber = 'M' + Math.floor(Math.random() * 1000000).toString().padStart(6, '0')
+const randomNumber = 'M' + Math.floor(Math.random() * 1000000).toString().padStart(4, '0')
 
 //generate random companyName
 const randomWords = generateRandomWords(2)
@@ -30,91 +30,119 @@ afterEach(() => {
 
 describe('Dashboard-Buttons', () => {
 
+    it('Check the existence of dashboard buttons', () => {
+        //Excel-Export-Button
+        cy.get('[href="/inquiries.xlsx"]')
+            .should('have.text', 'Excel Export')
+
+        //Partner importieren-Button
+        cy.get('[href="/partners/new"]')
+            .should('have.text', 'Partner importieren')
+
+        //Partner auflisten-Button
+        cy.get('[href="/partners"]')
+            .should('have.text', 'Partner auflisten')
+        
+        cy.log('All buttons exist')
+    })
+    
     it('Excel-Export', () => {
         //Excel-Exports-Button
         cy.get('[href="/inquiries.xlsx"]')
-            .should('have.text', 'Excel Export')
             .click()
             
         const today = new Date().toLocaleDateString('en-GB').split('/').join('.')
-        const filePath = `C:/Users/AlexanderSülzle¦aifi/Desktop/Risk_Decision_System/cypress/downloads/Anfragen bis ${today}.xlsx`
+        const filePath = `C:/Users/AlexanderSülzle¦aifi/Desktop/Risk_Decision_System_E2E_Test/cypress/downloads/Anfragen bis ${today}.xlsx`
 
         cy.readFile(filePath)
             .should('exist')
-    })
-    
-    it('Partner importieren', () => {
-        //Partner importieren-Button
-        cy.get('[href="/partners/new"]')
-            .should('contain', 'Partner importieren')
-            .click()
-        cy.url(`${baseUrl}/partners/new`)
-
-        //type mandatory number
-        cy.get('[name="external_id"]')
-            .should('not.have.value')
-            .type(randomNumber)
-        
-        //type company name
-        cy.get('[name="name"]')
-            .should('not.have.value')
-            .type(companyName)
-        
-        //select company type
-        const randomIndex = Math.floor(Math.random() * 8) + 1
-        cy.get(`:nth-child(3) > .col-sm-9 > :nth-child(${randomIndex})`) //lines of company types randomly selected
-            .find('.form-check-input')
-            .click()
-            //if randomIndex is 8 ("Sonstiges"), then add text
-            .then(() => { 
-            if (randomIndex === 8) {
-                cy.get('[name="company_type_addition"]')
-                    .type('Unternehmensform X')
-            }
-        })
-        
-        //select contract branch
-        const randomCheckboxIndex = Math.floor(Math.random() * 2) + 1
-        cy.get(`:nth-child(4) > .col-sm-9 > :nth-child(${randomCheckboxIndex})`)
-            .find('.form-check-input')
-            .click()
-
-        //Save-Button
-        cy.get('[data-react-class="forms/PartnerForm"] > form > [type="submit"]')
-            .should('contain', 'Speichern')
-            .click()
-        cy.url(`${baseUrl}/partners/´`)
+        cy.log('Excel-Export works')
     })
 
     it('Partner auflisten', () => {
         cy.get('[href="/partners"]')
-            .should('contain', 'Partner auflisten')
             .click()
         cy.url(`${baseUrl}/partners`)
         
-        // Search for companyName and randomNumber
-        cy.contains(companyName)
+        //check table headers
+        cy.get('thead > tr > :nth-child(1)')
+            .should('have.text', 'Name')
+        cy.get('thead > tr > :nth-child(2)')
+            .should('have.text', 'External')
+        
+        //check table content
+        cy.get('tbody')
             .should('exist')
-            .parent()
-            .find(`td:contains('${randomNumber}')`)
-            .siblings('td')
-            .find('a')
-            .first()
+            .find('tr')
+            .should('have.length.gt', 0)
+
+        //partner view button in first line
+        cy.get('tbody > :nth-child(1) > :nth-child(3)')
             .click()
         cy.url(`${baseUrl}/partners/´`)
+        cy.log('Partner view button works')
+    })
 
-        //back to previous page
-        cy.go('back')
-        cy.contains(companyName)
-            .should('exist')
-            .parent()
-            .find(`td:contains('${randomNumber}')`)
-            .siblings('td')
-            .find('a')
-            .eq(1)
-            .contains('Edit')
+    it('Partner importieren', () => {
+        cy.get('[href="/partners/new"]')
             .click()
-        cy.url(`${baseUrl}/partners/´/edit`)
+        cy.url(`${baseUrl}/partners/new`)
         
+        //fill out form
+        cy.get('#partner_external_id')
+            .type(randomNumber)
+        cy.get('.col-sm-1 > .btn') //Importieren-Button
+            .should('contain', 'Importieren')
+            .click()
+        if (cy.get('[data-partner-target="previouslyImported"]').should('not.be.visible')) {
+            cy.get('#partner_name')
+                .should('be.empty')
+                .type(companyName)
+
+            //type of company
+            const randomCompanyType = getRandomCompanyType()
+            function getRandomCompanyType() {
+                const companyTypes = ['Einzelunternehmen', 'GmbH', 'GbR', 'OHG', 'KG', 'UG', 'AG', 'Sonstige']
+                const randomIndex = Math.floor(Math.random() * companyTypes.length)
+                return companyTypes[randomIndex]
+            }
+            cy.get('#partner_company_type')
+                .select(randomCompanyType)
+
+            //type of contract
+            const randomContract = getRandomContract()
+            function getRandomContract() {
+                const contracts = ['Retail', 'Mittelstand']
+                const randomIndex = Math.floor(Math.random() * contracts.length)
+                return contracts[randomIndex]
+            }
+            cy.get('#partner_contract_branch')
+                .select(randomContract)
+
+            //lead channel
+            const randomLeadChannel = getRandomLeadChannel()
+            function getRandomLeadChannel() {
+                const leadchannel = ['Unbekannt', 'Direktanfrage', 'DFKP', 'Compeon', 'Fincompare', 'Unternehmensberater', 'Pflegeagentur', 'Sonstig']
+                const randomIndex = Math.floor(Math.random() * leadchannel.length)
+                return leadchannel[randomIndex]
+            }
+            cy.get('#partner_lead_channel')
+                .select(randomLeadChannel)
+
+            //crefo index
+            const randomCrefo = Math.floor(Math.random() * 501) + 100
+            cy.get('#partner_crefo_index')
+                .type(randomCrefo)
+            const today = new Date().toISOString().split('T')[0]
+            cy.get('#partner_crefo_index_date')
+                .type(today)
+        } else {
+            cy.get('[data-partner-target="previouslyImported"]')
+                .should('be.visible')
+            cy.get('[href="/partners/´"]')
+                .click()
+            cy.url(`${baseUrl}/partners/´`)
+        }
+
     })
 })
