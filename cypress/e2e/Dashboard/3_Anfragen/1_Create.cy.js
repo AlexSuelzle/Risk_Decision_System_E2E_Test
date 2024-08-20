9634c96a5db9e9f8b4043ec174e1538a6300b5e2
AlexSuelzle <145021047+AlexSuelzle@users.noreply.github.com>
2024-08-20 13:35:15 +0200

    create inquiry

--- cypress/e2e/Dashboard/3_Anfragen/1_Create.cy.js
+++ cypress/e2e/Dashboard/3_Anfragen/1_Create.cy.js
@@ -1,109 +1,165 @@
 import { partnerlist } from '../../Setup'
 
-const randomDecision = Cypress.env('randomDecision')
+const randomDecision = Cypress.env('randomDecision') //siehe cypress.config.json
+const baseUrl = Cypress.config('baseUrl') //siehe cypress.json
 
 beforeEach(() => {
-    cy.login() //see cypress/support/Login.js
-    partnerlist() //see cypress/e2e/Setup.js
+    cy.login() //siehe cypress/support/Login.js
+    partnerlist() //siehe cypress/e2e/Setup.js
 })
 
 afterEach(() => {
-    //cy.logout() //see cypress/support/Logout.js
+    cy.logout() //siehe cypress/support/Logout.js
 })
 
 describe('Partnerlist', () => {
 
     it('Create new inquiry', () => {
-        cy.get(':nth-child(5) > .card-header > .card-title')
-            .should('contain', 'Anfragen')
-            .find('.btn', 'Neue Anfrage')
+        cy.contains('Neue Anfrage')
             .click()
         cy.get('.modal-content')
             .should('be.visible')
         cy.get('.modal-header')
             .should('contain', 'Neue Anfrage anlegen')
-        
+
         //fill out form
         //select product type
-        const randomProductType = getRandomProductType()
-            function getRandomProductType() {
-                const productTypes = ['Factoring', 'Finetrading', 'Leasing']
-                return productTypes[Math.floor(Math.random() * productTypes.length)]
-            }
+        const randomProductType = getRandomItem(['Factoring', 'Finetrading', 'Leasing'])
         cy.get('#new-inquiry-product-type-select')
             .select(randomProductType)
-        
+
         //fill out form for selected product type
         if (randomProductType === 'Factoring') {
-            cy.log('Factoring selected')
-            //fill out form for factoring
-            //select inquiry type
-            const randomInquiryType = getRandomInquiryType()
-            function getRandomInquiryType() {
-                const inquiryTypes = ['Neu', 'Prolongation', 'Erhöhung']
-                return inquiryTypes[Math.floor(Math.random() * inquiryTypes.length)]
-            }            
-            cy.get('#new-inquiry-inquiry-type-select')
-                .select(randomInquiryType)
-            //fill out form for selected inquiry type
-            if (randomInquiryType === 'Neu') {
-                cy.log('Neu selected')
-                //fill out form for new inquiry
-                //Ankaufsrahmen
-                const randomAmount = Math.floor(Math.random() * (100000 - 100 + 1)) + 100
-                cy.get('#new-inquiry-form-credit-line-input')
-                    .should('be.empty')
-                    .type(randomAmount.toString())
-
-                //Sicherheitseinbehalt
-                const randomPercentage = Math.floor(Math.random() * (5 - 1 + 1)) + 1
-                cy.get('#inquiry_inquiry_entry_attributes_security_deposit')
-                    .should('be.empty')
-                    .type(randomPercentage.toString())
-
-                //Factoring type
-                const randomFactoringType = getRandomFactoringType()
-                function getRandomFactoringType() {
-                    const factoringTypes = ['Offen / Echt', 'Offen / Unecht', 'Still / Echt', 'Still / Unecht']
-                    return factoringTypes[Math.floor(Math.random() * inquiryTypes.length)]
-                }            
-                cy.get('#inquiry_inquiry_entry_attributes_factoring_type')
-                    .select(randomFactoringType)
-                cy.wait(500)
-
-                //decision between save or cancel
-                if (randomDecision) {
-                    cy.get('.modal-footer > .btn-primary')
-                        .should('contain', 'Anfrage anlegen')
-                        .click()
-                    cy.get('.modal-content')
-                        .should('not.be.visible')
-                    cy.url(`${baseUrl}/partners/´`)
-                } else {
-                    cy.get('.btn-secondary')
-                        .should('contain', 'Abbrechen')
-                        .click()
-                    cy.url(`${baseUrl}/partners/´`)
-                }
-
-            } else if (randomInquiryType === 'Prolongation') {
-                cy.log('Prolongation selected')
-                //fill out form for prolongation
-
-            } else {
-                cy.log('Erhöhung selected')
-                //fill out form for increase
-
-            }
-
-
+            fillOutFactoringForm()
         } else if (randomProductType === 'Finetrading') {
-            cy.log('Finetrading selected')
-
-
-
+            fillOutFinetradingForm()
         } else {
-            cy.log('Leasing selected')
+            fillOutLeasingForm()
         }
-    })
+
+        //decision between save or cancel
+        if (randomDecision) {
+            cy.get('.modal-footer > .btn-primary')
+                .should('contain', 'Anfrage anlegen')
+                .click()
+            cy.get('.modal-content')
+                .should('not.be.visible')
+            } else {
+                cy.get('.btn-secondary')
+                .should('contain', 'Abbrechen')
+                .click()
+                cy.url(`${baseUrl}/partners/´`)
+            }
+        })
+
+    function fillOutFactoringForm() {
+        cy.log('Factoring selected')
+        const randomInquiryType = getRandomItem(['Neu', 'Prolongation', 'Erhöhung'])
+        cy.get('#new-inquiry-inquiry-type-select')
+            .select(randomInquiryType)
+
+        if (randomInquiryType === 'Neu') {
+            cy.log('Neu selected')
+            fillOutNewInquiryForm()
+            cy.get('#inquiry_inquiry_entry_attributes_factoring_type')
+                .select(getRandomItem(['Offen / Echt', 'Offen / Unecht', 'Still / Echt', 'Still / Unecht']))
+        } else if (randomInquiryType === 'Prolongation') {
+            cy.log('Prolongation selected')
+            fillOutProlongationForm()
+        } else {
+            cy.log('Erhöhung selected')
+            fillOutIncreaseForm()
+        }
+    }
+
+    function fillOutFinetradingForm() {
+        cy.log('Finetrading selected')
+        const randomInquiryType = getRandomItem(['Neu', 'Prolongation', 'Erhöhung'])
+        cy.get('#new-inquiry-inquiry-type-select')
+            .select(randomInquiryType)
+
+        if (randomInquiryType === 'Neu') {
+            cy.log('Neu selected')
+            fillOutNewInquiryForm()
+            cy.get('#inquiry_inquiry_entry_attributes_factoring_type')
+                .select(getRandomItem(['Fertigprodukte', 'Hilfestoffe (Nebenbestandteil des Nebenproduktes)', 'Betriebsstoffe', 'Rohstoffe']))
+        } else if (randomInquiryType === 'Prolongation') {
+            cy.log('Prolongation selected')
+            fillOutProlongationForm()
+        } else {
+            cy.log('Erhöhung selected')
+            fillOutIncreaseForm()
+        }
+    }
+
+    function fillOutLeasingForm() {
+        cy.log('Leasing selected')
+        const randomInquiryType = getRandomItem(['Neu', 'Prolongation', 'Erhöhung'])
+        cy.get('#new-inquiry-inquiry-type-select')
+            .select(randomInquiryType)
+
+        if (randomInquiryType === 'Neu') {
+            cy.log('Neu selected')
+            fillOutNewInquiryForm()
+            cy.get('#inquiry_inquiry_entry_attributes_contract_type')
+                .select(getRandomItem(['MK', 'TA', 'VA']))
+        } else if (randomInquiryType === 'Prolongation') {
+            cy.log('Prolongation selected')
+            fillOutProlongationForm()
+        } else {
+            cy.log('Erhöhung selected')
+            fillOutIncreaseForm()
+        }
+    }
+
+    function fillOutNewInquiryForm() {
+        const randomAmount = getRandomNumber(100, 100000)
+        cy.get('#new-inquiry-form-credit-line-input')
+            .should('be.empty')
+            .type(randomAmount.toString())
+
+        if (randomProductType === 'Factoring') {
+            const randomPercentage = getRandomNumber(1, 5)
+            cy.get('#inquiry_inquiry_entry_attributes_security_deposit')
+                .should('be.empty')
+                .type(randomPercentage.toString())
+        } else if (randomProductType === 'Finetrading') {
+            const randomDays = getRandomNumber(10, 120)
+            cy.get('#inquiry_inquiry_entry_attributes_financing_term')
+                .should('be.empty')
+                .type(randomDays.toString())
+        } else {
+            const randomDays = getRandomNumber(10, 120)
+            cy.get('#inquiry_inquiry_entry_attributes_financing_term')
+                .should('be.empty')
+                .type(randomDays.toString())
+        }
+    }
+
+    function fillOutProlongationForm() {
+        const randomAmount = getRandomNumber(100, 100000)
+        cy.get('#new-inquiry-form-credit-line-input')
+            .should('be.empty')
+            .type(randomAmount.toString())
+
+        const randomDays = getRandomNumber(10, 120)
+        cy.get('#inquiry_inquiry_entry_attributes_financing_term')
+            .should('be.empty')
+            .type(randomDays.toString())
+    }
+
+    function fillOutIncreaseForm() {
+        const randomAmount = getRandomNumber(100, 100000)
+        cy.get('#inquiry_inquiry_entry_attributes_financial_framework')
+            .should('be.empty')
+            .type(randomAmount.toString())
+    }
+
+    function getRandomItem(items) {
+        return items[Math.floor(Math.random() * items.length)]
+    }
+
+    function getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
 })
