d04a390acf42dd5e2aaa0bd34912255f664f65b5
AlexSuelzle <145021047+AlexSuelzle@users.noreply.github.com>
2024-08-29 13:28:45 +0200

    update inquiry workflow

--- cypress/e2e/Inquiry_Workflow/1_Inquiry_Workflow.cy.js
+++ cypress/e2e/Inquiry_Workflow/1_Inquiry_Workflow.cy.js
@@ -4,6 +4,7 @@ const baseUrl = Cypress.config('baseUrl') //siehe cypress.json
 
 beforeEach(() => {
     cy.login() //siehe cypress/support/Login.js
+    cy.wait(2000)
     //inquiryview() //see cypress/e2e/Setup.js
 })
 
@@ -30,15 +31,15 @@ describe('Inquiry Workflow', () => {
         const selectedSurety = randomSurety[randomIndex]
         cy.get('#new-inquiry-surety-select')
             .select(selectedSurety)
-        
+        //
         if (selectedSurety === 'Bürgschaft') {
             cy.get('#inquiry_inquiry_guarantees_attributes_0_natural_person_id')
-                .select('Arthur Goldberg');
+                .select('Arthur Goldberg')
             cy.get('#inquiry_inquiry_guarantees_attributes_0_guarantee')
-                .type(Math.floor(Math.random() * (100000 - 100 + 1)) + 100);
+                .type(Math.floor(Math.random() * (100000 - 100 + 1)) + 100)
         } else if (selectedSurety === 'Grundschuld') {
             cy.get('#inquiry_land_charge')
-                .type(Math.floor(Math.random() * (100000 - 100 + 1)) + 100);
+                .type(Math.floor(Math.random() * (100000 - 100 + 1)) + 100)
         }
 
         //inquiry save button
@@ -49,10 +50,13 @@ describe('Inquiry Workflow', () => {
             .should('contain', 'Anfrage wurde aktualisiert.')
         cy.log('Inquiry save button works')
 
+
         //Jahresabschlüsse (financial statements)
-        cy.get('[href="/partners/*/inquiries/*/annual_accounts"]')
+        cy.contains('Jahresabschlüsse')
             .click()
-        cy.url(`${baseUrl}/partners/*/inquiries/*/annual_accounts`)
+        cy.url(`${baseUrl}/partners/´/inquiries/´/annual_accounts`)
+
+        //add new annual account
         cy.get('.show > .btn')
             .should('contain', 'Jahresabschluss hinzufügen')
             .click()
@@ -62,13 +66,13 @@ describe('Inquiry Workflow', () => {
         cy.get('.show > .row > :nth-child(1) > #annual_account_document_reception_date')
             .type(yesterday.toISOString().split('T')[0])
         //select months
-        const randomMonth = Math.floor(Math.random() * 12) + 1
         const monthNames = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
-        const selectedMonth = monthNames[randomMonth - 1]
+        const randomMonth = Math.floor(Math.random() * 12)
+        const selectedMonth = monthNames[randomMonth]
         cy.get('.show > .row > :nth-child(2) > #annual_account_from_month')
-            .select(selectedMonth)
+            .select(selectedMonth.toString())
         cy.get('.show > .row > :nth-child(4) > #annual_account_until_month')
-            .select(selectedMonth + 1)
+            .select(selectedMonth.toString())
         //select years
         const currentYear = new Date().getFullYear()
         cy.get('.show > .row > :nth-child(3) > #annual_account_from_year')
@@ -78,19 +82,50 @@ describe('Inquiry Workflow', () => {
             .clear()
             .type(currentYear)
         //GuV
-        const randomValue1 = Math.floor(Math.random() * (100000 - 100 + 1)) + 100
-        const randomValue2 = Math.floor(Math.random() * (100000 - 100 + 1)) + 100
+        const randomValue = Math.floor(Math.random() * (100000 - 100 + 1)) + 100
         cy.get(':nth-child(6) > #annual_account_liabilities_GuV_1')
-            .type(randomValue1)
-        cy.get(':nth-child(10) > .form-control > .js-ja-calculate-target')
-            .should('contain', randomValue1)
-        cy.get(':nth-child(11) > #annual_account_liabilities_GuV_5')
-            .type(randomValue2)
-        cy.get(':nth-child(15) > .form-control > .js-ja-calculate-target')
-            .should('contain', randomValue1 + randomValue2)
+            .type(randomValue)
         //save new annual account
+        cy.contains('Speichern')
+            .click()
+        cy.log('New annual account created')
+        cy.contains('Löschen')
+            .click()
+        cy.log('New annual account deleted')
+
+
+        //BWA
+        cy.contains('BWA')
+            .click()
+        cy.url(`${baseUrl}/partners/´/inquiries/´/bussiness_statements`)
+
+        //add new BWA
+        cy.contains('BWA hinzufügen')
+            .click()
+        //select month
+        cy.get('.show > .row > :nth-child(2) > #business_assessment_from_month')
+            .select(selectedMonth.toString())
+        cy.get('.show > .row > :nth-child(4) > #business_assessment_until_month')
+            .select(selectedMonth.toString())
+        //select years
+        cy.get('.show > .row > :nth-child(3) > #business_assessment_from_year')
+            .clear()
+            .type(currentYear)
+        cy.get('.show > .row > :nth-child(5) > #business_assessment_until_year')
+            .clear()
+            .type(currentYear)
+        //Umsatzerlöse
+        cy.get(':nth-child(4) > #business_assessment_liabilities_bwa_A')
+            .type(randomValue)
+        //save new BWA
         cy.get('.show > .text-center > .btn-primary')
             .should('contain', 'Speichern')
             .click()
+        cy.log('New BWA created')
+        //delete BWA
+        cy.get('[title="Löschen"]')
+            .first()
+            .click()
+        cy.log('New BWA deleted')
     })
 })
